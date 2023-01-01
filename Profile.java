fec87d9c3e2432c2c90991ddd7be369e13edee2b
codesche <110509654+codesche@users.noreply.github.com>
2023-01-01 15:50:31 +0900

    Create Profile.java

---
+++ Profile.java
@@ -0,0 +1,18 @@
+
+// 클래스
+public class Profile {
+    // main
+    public static void main (String[] args) {
+        // 변수 생성 및 값 대입
+        String name = "홍길동";
+        int studentNumber = 2022123104;
+        double tall = 1.75;
+        boolean isMale = true;
+
+        // 문자열 출력
+        System.out.println("이름: " + name);
+        System.out.println("학번: " + studentNumber);
+        System.out.println("신장: " + tall);
+        System.out.println("남자?: " + isMale);
+    }
+}
