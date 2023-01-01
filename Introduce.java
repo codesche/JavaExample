6b52d7e138c81e43e9aa2212726d443314c2a3c8
codesche <110509654+codesche@users.noreply.github.com>
2023-01-01 15:45:06 +0900

    Create Introduce.java

---
+++ Introduce.java
@@ -0,0 +1,11 @@
+
+// 클래스 생성
+public class Introduce {
+    // 메인 메소드 추가
+    public static void main(String[] args) {
+        System.out.println("안녕하세요 반갑습니다.");
+        System.out.println("저는 열혈 청년 김철수입니다.");
+        System.out.println("잘 부탁 드립니다!");
+    }
+
+}
